14b84379bf01618019df162a3a829b699e8c4ece
Samuel Henrique <94930912+samuhkls@users.noreply.github.com>
2024-10-25 22:04:08 -0300

    Add files via upload

---
+++ Exercicios/src/exercicios/exercicio1/Boletim.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package exercicios.exercicio1;
+
+/**
+ *
+ * @author samuel.hssantos4
+ */
+public interface Boletim {
+    public double calcularMedia(String nome, double p1, double p2);
+    public void publicar(String nome, double media);
+}
---
+++ Exercicios/src/exercicios/exercicio1/Professor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicios.exercicio1;
+
+/**
+ *
+ * @author samuel.hssantos4
+ */
+public class Professor implements Funcionario{
+    private String nome;
+    
+
+
+    @Override
+    public void atualizar(String nome, double media) {
+        double mediapub = calcularMedia(nome, p1, p2);
+    }
+    
+}
